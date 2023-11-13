e1e079ccb74bdb51247dffc051cf18b247cec8b9
VitaliiChichuga <chichygav@gmail.com>
2023-11-13 23:21:26 +0200

    qqq

--- js/script-mob.js
+++ js/script-mob.js
@@ -5,7 +5,7 @@ const dropMob = document.querySelector('.drop-mob');
 const cardItem = document.querySelectorAll('.card__item');
 const input = document.querySelectorAll('.input-btn');
 
-document.body.addEventListener('click', function () {
+addEventListener('click', function () {
   input.forEach(el => {
     if (el.checked) {
       dropMob.classList.add('drop-mob-show');
